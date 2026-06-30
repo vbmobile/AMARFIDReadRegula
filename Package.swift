afdfe49354c2f394ebe2cf01e0b2eb704bf6ad87
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-06-30 11:03:05 +0100

    Update binary to 1.0.0.3, fix product target export

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "AMARFIDReadRegula",
-            targets: ["AMARFIDReadRegulaWrapper"]
+            targets: ["AMARFIDReadRegula", "AMARFIDReadRegulaWrapper"]
         )
     ],
     dependencies: [
