aad2817a412ce79029a1e43f8a13410c057fb32a
Tiago Silva <tiago.silva@vision-box.com>
2026-09-11 12:22:00 +0100

    Publish AMARFIDReadRegula 1.1.2 package manifest

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AMARFIDReadRegula",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMARFIDReadRegula/AMARFIDReadRegula-1.1.1.zip",
-            checksum: "1ec5b1c9ecbdd55d5f0d62bc81c1582d1c3c4995e3bc622f62908c3b7ded1859"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMARFIDReadRegula/AMARFIDReadRegula-1.1.2.zip",
+            checksum: "0ea3117df309b8ab0c1cc154c7b50ff5405fb48a9f4ddc05779c660e185ede40"
         ),
         .target(
             name: "AMARFIDReadRegulaWrapper",
